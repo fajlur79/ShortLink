bad3965f59d0f88a4023650ba20623e5707eee96
Fajlur <fajlurgit@gmail.com>
2025-12-14 13:20:52 +0530

    fix setup script

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -11,17 +11,17 @@ chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 mkdir -p ~/Shortener/code/nginx
 
-cat <<'EOT' > ~/Shortener/code/nginx/nginx.conf
+cat <<'EOT' > /home/ec2-user/Shortener/code/nginx/nginx.conf
 ${nginx_config}
 EOT
 
-cat <<'EOT' > ~/Shortener/code/docker-compose.yml
+cat <<'EOT' > /home/ec2-user/Shortener/code/docker-compose.yml
 ${docker_compose_config}
 EOT
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-MY_IP=$(curl -H "X-aws-ec2-metadeta-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-ipv4)
+MY_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "PUBLIC_IP=$MY_IP" >~/Shortener/code/.env
 
-chown -R ec2-user:ec2-user ~/Shortener
+chown -R ec2-user:ec2-user /home/ec2-user/Shortener
 
