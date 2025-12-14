d7125c6e8bae438153282808b347daf12e440931
Fajlur <fajlurgit@gmail.com>
2025-12-14 13:24:42 +0530

    fix setup script

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -9,7 +9,7 @@ mkdir -p /usr/local/lib/docker/cli-plugins/
 curl -SL  https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
 chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
-mkdir -p ~/Shortener/code/nginx
+mkdir -p /home/ec2-user/Shortener/code/nginx
 
 cat <<'EOT' > /home/ec2-user/Shortener/code/nginx/nginx.conf
 ${nginx_config}
