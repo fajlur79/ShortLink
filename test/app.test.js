742705e4a9ba7676cfa7030adcf9791e50e26863
Fajlur <fajlurgit@gmail.com>
2025-12-24 20:01:22 +0530

    Frontend intregation

--- test/app.test.js
+++ test/app.test.js
@@ -2,13 +2,9 @@ import request from 'supertest';
 import app from '../app.js';
 
 describe('GET /', () => {
-    it('should return 200 OK and a health check message', async () => {
-        const res = await request(app).get('/');
-        
-       
-        expect(res.statusCode).toBe(200);
-        
-        
-        expect(res.text).toContain("Welcome to ShortLink."); 
-    });
+    it('should return 200 OK and the frontend page', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain("ShortLink | Professional URL Shortener"); 
+});
 });
